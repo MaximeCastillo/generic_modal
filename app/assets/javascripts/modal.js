0795001756ef311ba98db17231ca0a160d4f2077
Maxime Castillo <maxime@hop3team.com>
2024-08-19 15:09:24 +0200

    Add an example modal

---
+++ app/assets/javascripts/modal.js
@@ -0,0 +1,15 @@
+
+function openModal() {
+  var modal = document.getElementById('gmModal');
+  modal.style.display = "block";
+}
+function closeModal() {
+  var modal = document.getElementById('gmModal');
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  var modal = document.getElementById('gmModal');
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
